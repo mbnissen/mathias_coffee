41aea7f5da09091baba77cdfba84ab2e4da3119d
mbnissen <morten@endavu.com>
2025-01-03 10:21:23 +0100

    Use new version of Elixir

--- test/mathias_coffee_web/controllers/error_html_test.exs
+++ test/mathias_coffee_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule MathiasCoffeeWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(MathiasCoffeeWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(MathiasCoffeeWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
